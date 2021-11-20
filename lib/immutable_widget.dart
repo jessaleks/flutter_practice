94951316800e51f1b02daa0301a1cc40cb187f8d
Aleksander Jess <jess.aleksander@gmail.com>
2021-11-20 13:52:00 +0100

    drawing a circle

--- lib/immutable_widget.dart
+++ lib/immutable_widget.dart
@@ -8,17 +8,33 @@ class ImmutableWidget extends StatelessWidget {
     return Container(
       color: Colors.green,
       child: Padding(
-        padding: EdgeInsets.all(40.0),
+        padding: const EdgeInsets.all(40.0),
         child: Container(
           color: Colors.purple,
           child: Padding(
             padding: const EdgeInsets.all(50.0),
-            child: Container(
-              color: Colors.blue,
-            ),
+            child: _buildShinyCircle(),
           )
         ),
       )
+    );
+  }
+
+  Widget _buildShinyCircle() {
+    return Container(
+      decoration: const BoxDecoration(
+        shape: BoxShape.circle,
+        gradient: RadialGradient(
+          colors: [
+            Colors.lightBlueAccent,
+            Colors.blueAccent,
+          ],
+          center: Alignment(-0.3, -0.5)
+        ),
+        boxShadow: [
+          BoxShadow(blurRadius: 20)
+        ]
+      ),
     );
   }
 }

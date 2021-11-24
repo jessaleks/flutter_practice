e56a59bb6acea1cbb6b9a6b2a842d9204f94103e
Aleksander Jess <jess.aleksander@gmail.com>
2021-11-24 21:24:52 +0100

    added some basic text styling in a widget

--- lib/text_layout.dart
+++ lib/text_layout.dart
@@ -1,1 +1,17 @@
-a
+import 'package:flutter/material.dart';
+
+class TextLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        const Text('Hello, World', style: TextStyle(fontSize: 16)),
+        Text('Text can wrap without issue',
+            style: Theme.of(context).textTheme.headline6),
+        const Text(
+            'Hello, World Hello, World Hello, World Hello, World Hello, World Hello, World Hello, World Hello, World Hello, World '),
+      ],
+    );
+  }
+}

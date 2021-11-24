fae7908199ea7da03d8482ac2f8af9ae75186ffa
Aleksander Jess <jess.aleksander@gmail.com>
2021-11-24 21:50:45 +0100

    fixing the error

--- lib/text_layout.dart
+++ lib/text_layout.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class TextLayout extends StatelessWidget {
+  TextLayout({Key? key}) : super(key: key) {}
+
   @override
   Widget build(BuildContext context) {
     return Column(

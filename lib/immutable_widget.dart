d8a975507d1f8057427de9ba3b705abd97c61724
Aleksander Jess <jess.aleksander@gmail.com>
2021-11-27 19:57:35 +0100

    finished chapter one

--- lib/immutable_widget.dart
+++ lib/immutable_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import "dart:math" as math;
 
 import 'package:flutter_practice/text_layout.dart';
 
@@ -10,19 +9,10 @@ class ImmutableWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[Image.asset('assets/713678.jpg'), TextLayout()],
-    );
-  }
-
-  Widget _buildShinyCircle() {
-    return Container(
-      decoration: const BoxDecoration(
-          shape: BoxShape.circle,
-          gradient: RadialGradient(colors: [
-            Colors.lightBlueAccent,
-            Colors.blueAccent,
-          ], center: Alignment(-0.3, -0.5)),
-          boxShadow: [BoxShadow(blurRadius: 20)]),
+      children: <Widget>[
+        Image.asset('assets/713678.jpg'),
+        Center(child: TextLayout())
+      ],
     );
   }
 }

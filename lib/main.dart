e4ba4a26550cca25083c20e9c918620f014a5630
Aleksander Jess <jess.aleksander@gmail.com>
2021-11-28 14:38:41 +0100

    changes to the app - building a profile screen

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,10 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: ProfileScreen()
+      home: ProfileScreen(),
     );
   }
 }
--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -5,24 +5,87 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Stack(
+      clipBehavior: Clip.antiAliasWithSaveLayer,
       children: [
-        _buildProfileImage(context),
-        _buildProfileDetails(context),
-        _buildActions(context)
+        Image.asset('assets/713678.jpg'),
+        Transform.translate(
+          offset: const Offset(0, 100),
+          child: Column(
+            children: [
+              _buildProfileImage(context),
+              _buildProfileDetails(context),
+              _buildActions(context)
+            ],
+          ),
+        )
       ],
     );
   }
 
   Widget _buildProfileImage(BuildContext context) {
-    return Container();
+    return SizedBox(
+        width: 200,
+        height: 200,
+        child: ClipOval(
+            child: Image.asset(
+          'assets/713678.jpg',
+          fit: BoxFit.cover,
+        )));
   }
 
   Widget _buildProfileDetails(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "Lorem Ipsum",
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+          ),
+          _buildDetailsRow('Age', '4'),
+          _buildDetailsRow('Status', 'Good Boy')
+        ],
+      ),
+    );
   }
 
   Widget _buildActions(BuildContext context) {
-    return Container();
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildIcon(Icons.restaurant, 'Feed'),
+        _buildIcon(Icons.favorite, 'Pet'),
+        _buildIcon(Icons.directions_walk, 'Walk'),
+      ],
+    );
   }
+}
+
+Widget _buildDetailsRow(String heading, String value) {
+  return Row(
+    children: <Widget>[
+      Text(
+        '$heading: ',
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+          overflow: TextOverflow.fade,
+          fontSize: 15,
+        ),
+      ),
+      Text(value, softWrap: true, style: const TextStyle(fontSize: 15)),
+    ],
+  );
+}
+
+Widget _buildIcon(IconData icon, String text) {
+  return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          Icon(icon, size: 40),
+          Text(text, style: const TextStyle(fontSize: 20))
+        ],
+      ));
 }

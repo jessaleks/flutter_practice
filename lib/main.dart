b2400cceabc2453e36a2c4d34020f1540fa0fc13
Aleksander Jess <jess.aleksander@gmail.com>
2021-11-28 21:57:25 +0100

    the Flexible and the Expanded widgets

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      key: Key("key"),
+    return MaterialApp(
+      key: const Key("key"),
       home: FlexScreen(),
     );
   }

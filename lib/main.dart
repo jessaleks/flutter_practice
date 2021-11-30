dc3bfaef72ce1f36143def65d5f69171922d9c26
Aleksander Jess <jess.aleksander@gmail.com>
2021-11-30 22:29:30 +0100

    everything working now

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
 import './stopwatch.dart';
-void main() => runApp(StopWatchApp())
 
+void main() => runApp(const StopWatchApp());
 
 class StopWatchApp extends StatelessWidget {
-
   const StopWatchApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: StopWatch(
-        key: Key("key")
-      )
-    )
+    return const MaterialApp(home: StopWatch(key: Key("key")));
   }
 }
